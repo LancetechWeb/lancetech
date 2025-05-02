3578b10a76884f74b22e9972e627a57e4965843b
OSABHIE FERGUSON <ebenezerosas92@gmail.com>
2025-05-02 02:07:41 -0400

    feat: token logic

--- src/admin-dashboard/components/AdminLogin.tsx
+++ src/admin-dashboard/components/AdminLogin.tsx
@@ -1,100 +1,98 @@
-import { Box, Button, Typography } from '@mui/material'
-import React, { useEffect, useRef } from 'react'
-import axios, { AxiosError } from 'axios'
-import { darkBackgroundTextFieldStyles } from '../../core/styles/textField.styles'
-import { COLORS } from '../../core/styles/COLORS'
-import { useDispatch } from 'react-redux'
-import { setIsAuthenticated, setSnackbar, setUser } from '../../core/reducers/coreSlice'
-import Logo from '../../core/components/Logo'
-import { loginComponentStyles } from '../styles/admin.styles'
-import { useNavigate } from 'react-router-dom'
-import { getVariable } from '../../utils/misc/env.misc'
-import TextFieldComponent from '../../form-fields/components/TextFieldComponent'
-import { useForm } from 'react-hook-form'
-import { AdminLoginFormType } from '../types/dashboard.types'
+import { Box, Button, Typography } from '@mui/material';
+import React, { useEffect, useRef } from 'react';
+import axios, { AxiosError } from 'axios';
+import { darkBackgroundTextFieldStyles } from '../../core/styles/textField.styles';
+import { COLORS } from '../../core/styles/COLORS';
+import { useDispatch } from 'react-redux';
+import { setIsAuthenticated, setSnackbar, setUser } from '../../core/reducers/coreSlice';
+import Logo from '../../core/components/Logo';
+import { loginComponentStyles } from '../styles/admin.styles';
+import { useNavigate } from 'react-router-dom';
+import { getVariable } from '../../utils/misc/env.misc';
+import TextFieldComponent from '../../form-fields/components/TextFieldComponent';
+import { useForm } from 'react-hook-form';
+import { AdminLoginFormType } from '../types/dashboard.types';
 
 const AdminLogin = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const today = new Date()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const today = new Date();
 
-  const handleLogin = async (formValues:AdminLoginFormType) => {
-  
-      const postData = formValues
-  
-      try {
-        const response = await axios.post(`${getVariable("BASE_URL")}/authenticate/sign-in`, postData,
-          {
-            withCredentials: true,
-            headers: {
-              'Cache-Control': 'no-cache',
-              'Pragma': 'no-cache'
-            }
-          },
-        );
+  const handleLogin = async (formValues: AdminLoginFormType) => {
+    const postData = formValues;
 
-        if(response.status === 200){
-          dispatch(setIsAuthenticated(true))
-          dispatch(setUser(response.data))
-          dispatch(setSnackbar({type:"success", message:"logged in successfully"}))
-          navigate('/admin/dashboard')
-        }
-      } catch (error) {
-        if (error instanceof AxiosError) 
-          dispatch(setSnackbar({type:"error", message:`oops! there was an error signing in: ${error.response?.data.message}`}))
-        else 
-          dispatch(setSnackbar({type:"error", message:`oops! an unknownn error occurred: ${error}`}))
+    try {
+      const response = await axios.post(`${getVariable('BASE_URL')}/authenticate/sign-in`, postData, {
+        withCredentials: true,
+        headers: {
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+        },
+      });
+
+      if (response.status === 200) {
+        const { token, user } = response.data;
+        localStorage.setItem('token', token);
+        dispatch(setIsAuthenticated(true));
+        dispatch(setUser(user));
+        dispatch(setSnackbar({ type: 'success', message: 'logged in successfully' }));
+        navigate('/admin/dashboard');
       }
+    } catch (error) {
+      if (error instanceof AxiosError)
+        dispatch(setSnackbar({ type: 'error', message: `oops! there was an error signing in: ${error.response?.data.message}` }));
+      else dispatch(setSnackbar({ type: 'error', message: `oops! an unknownn error occurred: ${error}` }));
+    }
   };
 
   // custom hooks
   const { control, handleSubmit } = useForm<AdminLoginFormType>({
     defaultValues: {
-      email:'',
-      password:''
+      email: '',
+      password: '',
     },
   });
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if(event.key === "Enter") handleSubmit(handleLogin)
+    if (event.key === 'Enter') handleSubmit(handleLogin);
   };
 
-  const compRef = useRef<HTMLElement>()
+  const compRef = useRef<HTMLElement>();
 
   // to scroll to the bottom in phones
   // so the whole page can be seen
-  useEffect(()=>{
-    if(compRef.current)
-      compRef.current.scrollIntoView()   
-  }, [])
-    
-  return (
-    <Box 
-      sx={loginComponentStyles}
-    >
-      <Box> <Logo noText/> </Box>
+  useEffect(() => {
+    if (compRef.current) compRef.current.scrollIntoView();
+  }, []);
 
-      <Box 
-        sx={{ 
-          display:"flex", 
-          flexDirection:"column", 
-          alignItems:"center", 
-          justifyContent:"center", 
-          gap:3, 
-          py:10,
-          px:4,
-          border:`1px solid ${COLORS.MainBlue}`,
-          borderRadius:"5px",
-          width:"20%",
-          minWidth:"250px",
-          maxWidth:"380px"
+  return (
+    <Box sx={loginComponentStyles}>
+      <Box>
+        {' '}
+        <Logo noText />{' '}
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 3,
+          py: 10,
+          px: 4,
+          border: `1px solid ${COLORS.MainBlue}`,
+          borderRadius: '5px',
+          width: '20%',
+          minWidth: '250px',
+          maxWidth: '380px',
         }}
         component="form"
         onSubmit={handleSubmit(handleLogin)}
       >
-        <TextFieldComponent <AdminLoginFormType>
+        <TextFieldComponent<AdminLoginFormType>
           id="outlined-basic"
-          name='email'
+          name="email"
           label="Email"
           variant="outlined"
           control={control}
@@ -102,42 +100,42 @@ const AdminLogin = () => {
             style: { color: 'white' }, // Label color
           }}
           onKeyDown={handleKeyDown}
-          sx={darkBackgroundTextFieldStyles} 
-          rules={{required:"email is required"}}         
+          sx={darkBackgroundTextFieldStyles}
+          rules={{ required: 'email is required' }}
         />
 
-        <TextFieldComponent <AdminLoginFormType>
-          id="outlined-basic" 
-          name='password'
-          label="Password" 
-          type='password'
-          variant="outlined" 
+        <TextFieldComponent<AdminLoginFormType>
+          id="outlined-basic"
+          name="password"
+          label="Password"
+          type="password"
+          variant="outlined"
           control={control}
           InputLabelProps={{
             style: { color: 'white' }, // Label color
           }}
           onKeyDown={handleKeyDown}
           sx={darkBackgroundTextFieldStyles}
-          rules={{required:"password is required"}}         
+          rules={{ required: 'password is required' }}
         />
 
-        <Button variant="contained" type='submit' sx={{width:"100%", mt:5, py:2}}>
+        <Button variant="contained" type="submit" sx={{ width: '100%', mt: 5, py: 2 }}>
           Login
         </Button>
       </Box>
-      <Box ref={compRef} 
-          sx={{
-            justifySelf:"flex-start", 
-            position:"absolute", 
-            bottom:50,
-            color:COLORS.LightFont
-          }}>
-        <Typography variant='caption'>
-          © {today.getFullYear()} Lancetech Services, Ltd. All rights reserved.
-        </Typography>
+      <Box
+        ref={compRef}
+        sx={{
+          justifySelf: 'flex-start',
+          position: 'absolute',
+          bottom: 50,
+          color: COLORS.LightFont,
+        }}
+      >
+        <Typography variant="caption">© {today.getFullYear()} Lancetech Services, Ltd. All rights reserved.</Typography>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default AdminLogin
+export default AdminLogin;
--- src/utils/auth/axiosInstance.ts
+++ src/utils/auth/axiosInstance.ts
@@ -4,52 +4,51 @@ import { setIsAuthenticated, setUser } from '../../core/reducers/coreSlice';
 import { getVariable } from '../misc/env.misc';
 import router from '../../core/routes/routes';
 
-const currentPath = window.location.pathname
+const currentPath = window.location.pathname;
 
 const axiosInstance = axios.create({
-  baseURL: getVariable("BASE_URL"),
+  baseURL: getVariable('BASE_URL'),
   withCredentials: true, // If you're dealing with cookies or sessions
   headers: {
-      'Cache-Control': 'no-cache',            
-  } 
+    'Cache-Control': 'no-cache',
+  },
 });
 
 // Add request interceptor
 axiosInstance.interceptors.request.use(
-  (config) => {
+  config => {
     // Modify config (e.g., add auth token)
-    const token = localStorage.getItem('authToken');
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
-    console.error(error.message)
+  error => {
+    console.error(error.message);
 
-     // Prevent propagation of the error
-     return; // Simply return to handle the error within the interceptor
+    // Prevent propagation of the error
+    return; // Simply return to handle the error within the interceptor
   }
 );
 
 // Add response interceptor
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  response => response,
+  error => {
     // Handle errors globally
     if (error.response?.status === 401) {
       console.log('Unauthorized! Redirecting to login...');
       store.dispatch(setIsAuthenticated(false));
       store.dispatch(setUser(undefined));
 
-      if(currentPath.includes("admin/dashboard"))
-        router.navigate('/admin/login')
+      if (currentPath.includes('admin/dashboard')) router.navigate('/admin/login');
     }
 
-    console.log("...can see error")
+    console.log('...can see error');
 
-    console.error(error.message)
-    
+    console.error(error.message);
+
     // Prevent propagation of the error
     throw error; // Simply return to handle the error within the interceptor
   }
@@ -57,24 +56,24 @@ axiosInstance.interceptors.response.use(
 
 /**
  * extracts the data from the response or error from the axios request
- * @param config 
- * @returns 
+ * @param config
+ * @returns
  */
-export const axiosWrapper = async (config: AxiosRequestConfig): Promise<{ data?: any; error?: AxiosError|Error }> => {
+export const axiosWrapper = async (config: AxiosRequestConfig): Promise<{ data?: any; error?: AxiosError | Error }> => {
   try {
-    const response:AxiosResponse = await axiosInstance.request(config);
+    const response: AxiosResponse = await axiosInstance.request(config);
     return { data: response.data, error: undefined };
   } catch (error) {
-    console.error("error", error)
-    
-    if(error){
-      if (error instanceof AxiosError)  
-        return {data: undefined, error:{...error, message:error?.response?.data?.message ?? error.message}};
+    console.error('error', error);
 
-      if(error instanceof Error) return { data: undefined, error }
+    if (error) {
+      if (error instanceof AxiosError)
+        return { data: undefined, error: { ...error, message: error?.response?.data?.message ?? error.message } };
+
+      if (error instanceof Error) return { data: undefined, error };
     }
 
-    return { data: undefined, error:undefined }
+    return { data: undefined, error: undefined };
   }
 };
 
